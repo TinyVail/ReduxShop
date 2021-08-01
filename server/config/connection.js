894b1fa1ea3b0436152cfad8f95f89adf0d63528
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-08-01 16:05:36 -0500

    changed shopping

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const password = process.env.MONGODB_PASSWORD;
 
-mongoose.connect(process.env.MONGODB_URI ||
-   `mongodb+srv://TinyVail:${password}@cluster0.nf8bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    `mongodb+srv://TinyVail:${password}@cluster0.nf8bc.mongodb.net/mernshopping?retryWrites=true&w=majority`,
   {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
